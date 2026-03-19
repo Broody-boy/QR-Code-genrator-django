0dd5c543e8bce5038dc76397a04ccc8d18853832
Broody-boy <arshdeep2875@gmail.com>
2026-03-19 09:28:48 +0530

    deploying to vercel

--- generator/views.py
+++ generator/views.py
@@ -1,86 +1,56 @@
 from django.shortcuts import render
-from django.http import HttpResponse, FileResponse
+from django.http import HttpResponse
 import qrcode
 import io
-import os
-from django.conf import settings
-from datetime import datetime
+import base64
 
 
 def home(request):
-    """
-    Home page view - displays the form to generate QR codes
-    """
     return render(request, 'generator/home.html')
 
 
+def _make_qr_image(data):
+    """Generate QR code and return as PNG bytes."""
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    buffer = io.BytesIO()
+    img.save(buffer, format='PNG')
+    buffer.seek(0)
+    return buffer.read()
+
+
 def generate_qr(request):
-    """
-    Generate QR code based on user input and display it
-    """
     if request.method == 'POST':
-        # Get the data from the form
         data = request.POST.get('qr_data', '')
-        
         if data:
-            # Create QR code instance
-            qr = qrcode.QRCode(
-                version=1,  # controls the size of the QR code
-                error_correction=qrcode.constants.ERROR_CORRECT_L,
-                box_size=10,
-                border=4,
-            )
-            
-            # Add data to the QR code
-            qr.add_data(data)
-            qr.make(fit=True)
-            
-            # Create an image from the QR code
-            img = qr.make_image(fill_color="black", back_color="white")
-            
-            # Save the image temporarily
-            # Create media directory if it doesn't exist
-            media_dir = os.path.join(settings.MEDIA_ROOT, 'qrcodes')
-            os.makedirs(media_dir, exist_ok=True)
-            
-            # Generate a unique filename with timestamp
-            timestamp = datetime.now().strftime('%Y%m%d_%H%M%S_%f')
-            filename = f'qrcode_{timestamp}.png'
-            filepath = os.path.join(media_dir, filename)
-            
-            # Save the image
-            img.save(filepath)
-            
-            # Create the URL for the image
-            qr_code_url = f'{settings.MEDIA_URL}qrcodes/{filename}'
-            
+            png_bytes = _make_qr_image(data)
+            b64 = base64.b64encode(png_bytes).decode('utf-8')
             context = {
-                'qr_code_url': qr_code_url,
+                'qr_code_url': f'data:image/png;base64,{b64}',
                 'qr_data': data,
-                'filename': filename,
+                # pass data encoded for use in download URL
+                'qr_data_b64': base64.urlsafe_b64encode(data.encode()).decode(),
             }
-            
             return render(request, 'generator/result.html', context)
-    
-    # If not POST or no data, redirect to home
     return render(request, 'generator/home.html')
 
 
 def download_qr(request):
-    """
-    Download the generated QR code
-    """
-    if request.method == 'GET':
-        filename = request.GET.get('filename', '')
-        
-        if filename:
-            filepath = os.path.join(settings.MEDIA_ROOT, 'qrcodes', filename)
-            
-            if os.path.exists(filepath):
-                # Open the file and return it as a download
-                response = FileResponse(open(filepath, 'rb'), content_type='image/png')
-                response['Content-Disposition'] = f'attachment; filename="{filename}"'
-                return response
-    
-    # If something went wrong, redirect to home
+    data_b64 = request.GET.get('data', '')
+    if data_b64:
+        try:
+            data = base64.urlsafe_b64decode(data_b64.encode()).decode()
+            png_bytes = _make_qr_image(data)
+            response = HttpResponse(png_bytes, content_type='image/png')
+            response['Content-Disposition'] = 'attachment; filename="qrcode.png"'
+            return response
+        except Exception:
+            pass
     return render(request, 'generator/home.html')
--- qrcode_project/settings.py
+++ qrcode_project/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qr-code-generator-secret-key-change-in-production'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-qr-code-generator-secret-key-change-in-production')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,10 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-
-# Media files (Generated QR codes)
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
